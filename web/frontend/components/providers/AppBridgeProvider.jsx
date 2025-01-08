948b4196d506a6c7268b33fb8e20bb39415253bc
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-01-08 10:29:58 +0530

    fix the problem of env variable

--- web/frontend/components/providers/AppBridgeProvider.jsx
+++ web/frontend/components/providers/AppBridgeProvider.jsx
@@ -4,6 +4,7 @@ import { Provider } from "@shopify/app-bridge-react";
 import { Banner, Layout, Page } from "@shopify/polaris";
 
 
+
 /**
  * A component to configure App Bridge.
  * @desc A thin wrapper around AppBridgeProvider that provides the following capabilities:
@@ -44,33 +45,33 @@ export function AppBridgeProvider({ children }) {
 
     return {
       host,
-      apiKey: process.env.SHOPIFY_API_KEY,
+      apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
       forceRedirect: true,
     };
   });
 
-  if (!process.env.SHOPIFY_API_KEY || !appBridgeConfig.host) {
-    const bannerProps = !process.env.SHOPIFY_API_KEY
+  if (!import.meta.env.VITE_SHOPIFY_API_KEY || !appBridgeConfig.host) {
+    const bannerProps = !import.meta.env.VITE_SHOPIFY_API_KEY
       ? {
-          title: "Missing Shopify API Key",
-          children: (
-            <>
-              Your app is running without the SHOPIFY_API_KEY environment
-              variable. Please ensure that it is set when running or building
-              your React app.
-            </>
-          ),
-        }
+        title: "Missing Shopify API Key",
+        children: (
+          <>
+            Your app is running without the SHOPIFY_API_KEY environment
+            variable. Please ensure that it is set when running or building
+            your React app.
+          </>
+        ),
+      }
       : {
-          title: "Missing host query argument",
-          children: (
-            <>
-              Your app can only load if the URL has a <b>host</b> argument.
-              Please ensure that it is set, or access your app using the
-              Partners Dashboard <b>Test your app</b> feature
-            </>
-          ),
-        };
+        title: "Missing host query argument",
+        children: (
+          <>
+            Your app can only load if the URL has a <b>host</b> argument.
+            Please ensure that it is set, or access your app using the
+            Partners Dashboard <b>Test your app</b> feature
+          </>
+        ),
+      };
 
     return (
       <Page narrowWidth>
